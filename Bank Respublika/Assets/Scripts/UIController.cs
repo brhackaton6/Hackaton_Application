403fea1dd3f4b7bbc198e119956f66fe58e1d730
BRHackaton6 <brhackaton6@amail.az>
2022-08-06 05:28:01 +0400

    BakiKartOptionAdded

--- Bank Respublika/Assets/Scripts/UIController.cs
+++ Bank Respublika/Assets/Scripts/UIController.cs
@@ -6,6 +6,8 @@ public class UIController : MonoBehaviour
 {
     [SerializeField] GameObject optionMenu;
     [SerializeField] GameObject optionMenuBG;
+    [SerializeField] GameObject addCardMenu;
+    [SerializeField] GameObject MainMenu;
     [SerializeField] Camera camera;
     RaycastHit2D hit;
     private bool GUIon;
@@ -13,6 +15,7 @@ public class UIController : MonoBehaviour
     {
         optionMenu.SetActive(true);
         optionMenuBG.SetActive(true);
+
 
     }
 
@@ -26,7 +29,26 @@ public class UIController : MonoBehaviour
     {
         optionMenu.SetActive(false);
         optionMenuBG.SetActive(false);
+        addCardMenu.SetActive(false);
+        MainMenu.SetActive(true);
     }
 
+
+    public void AddBakiKart()
+    {
+        optionMenu.SetActive(false);
+        optionMenuBG.SetActive(false);
+        addCardMenu.SetActive(true);
+        MainMenu.SetActive(false);
+    }
+
+    public void BackToMenu()
+    {
+        optionMenu.SetActive(false);
+        optionMenuBG.SetActive(false);
+        addCardMenu.SetActive(false);
+        MainMenu.SetActive(true);
+
+    }
 }
 
